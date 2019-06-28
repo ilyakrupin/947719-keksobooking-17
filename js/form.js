eebbf96600ae6f20aafd738d20ca1026df84e08e
Ilya Krupin <dev@krupibox.ru>
2019-06-28 13:00:31 +0300

    Add from module

--- js/form.js
+++ js/form.js
@@ -1,43 +1,43 @@
 'use strict';
 
-var adForm = document.querySelector('.ad-form');
-var selectType = adForm.querySelector('#type');
-var inputPrice = adForm.querySelector('#price');
+(function () {
+  var adForm = document.querySelector('.ad-form');
+  var selectType = adForm.querySelector('#type');
+  var inputPrice = adForm.querySelector('#price');
+  var selectRooms = adForm.querySelector('#room_number');
+  var selectCapacity = adForm.querySelector('#capacity');
+  var selectTimeIn = adForm.querySelector('#timein');
+  var selectTimeOut = adForm.querySelector('#timeout');
 
-var Types = {
-  'bungalo': 0,
-  'flat': 1000,
-  'house': 5000,
-  'palace': 10000
-};
+  var Types = {
+    'bungalo': 0,
+    'flat': 1000,
+    'house': 5000,
+    'palace': 10000
+  };
 
-selectType.addEventListener('change', function (evt) {
-  inputPrice.min = inputPrice.placeholder = Types[evt.target.value];
-});
+  selectType.addEventListener('change', function (evt) {
+    inputPrice.min = inputPrice.placeholder = Types[evt.target.value];
+  });
 
-var selectTimeIn = adForm.querySelector('#timein');
-var selectTimeOut = adForm.querySelector('#timeout');
+  selectTimeIn.addEventListener('change', function (evt) {
+    selectTimeOut.value = evt.target.value;
+    selectTimeOut[evt.currentTarget.selectedIndex].selected = true;
+  });
 
-selectTimeIn.addEventListener('change', function (evt) {
-  selectTimeOut.value = evt.target.value;
-  selectTimeOut[evt.currentTarget.selectedIndex].selected = true;
-});
+  selectTimeOut.addEventListener('change', function (evt) {
+    selectTimeIn.value = evt.target.value;
+  });
 
-selectTimeOut.addEventListener('change', function (evt) {
-  selectTimeIn.value = evt.target.value;
-});
+  selectRooms.addEventListener('change', function (evt) {
+    var index = evt.target.selectedIndex;
+    selectCapacity.value = selectCapacity[index].value;
+    selectCapacity[index].selected = true;
+  });
 
-var selectRooms = adForm.querySelector('#room_number');
-var selectCapacity = adForm.querySelector('#capacity');
-
-selectRooms.addEventListener('change', function (evt) {
-  var index = evt.target.selectedIndex;
-  selectCapacity.value = selectCapacity[index].value;
-  selectCapacity[index].selected = true;
-});
-
-selectCapacity.addEventListener('change', function (evt) {
-  var index = evt.target.selectedIndex;
-  selectRooms.value = selectRooms[index].value;
-  selectRooms[index].selected = true;
-});
+  selectCapacity.addEventListener('change', function (evt) {
+    var index = evt.target.selectedIndex;
+    selectRooms.value = selectRooms[index].value;
+    selectRooms[index].selected = true;
+  });
+})();

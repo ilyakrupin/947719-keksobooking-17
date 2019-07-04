e1dc08ab42b496e74bb65c33d3e96c99ebced0f2
Ilya Krupin <dev@krupibox.ru>
2019-07-04 14:36:59 +0300

    Update  filter

--- js/filters.js
+++ js/filters.js
@@ -5,22 +5,14 @@
   var map = document.querySelector('.map');
   var mapPins = document.querySelector('.map__pins');
   var typeFilter = map.querySelector('#housing-type');
+
   var onTypeFilterChange = function () {
     var mapPinsList = mapPins.querySelectorAll('button[id]');
 
-    [].filter.call(mapPinsList, function (element) {
-      element.style.visibility = 'hidden';
-      return typeFilter.value === element.id;
-    }).forEach(function (element, index) {
-      if (index < window.data.Pin.count) {
-        element.style.visibility = 'visible';
-      }
-    });
+    [].forEach.call(mapPinsList, function (element, index) {
+      element.style.visibility = (typeFilter.value === element.id) ? 'visible' : 'hidden';
 
-    [].filter.call(mapPinsList, function () {
-      return typeFilter.value === 'any';
-    }).forEach(function (element, index) {
-      if (index < window.data.Pin.count) {
+      if (typeFilter.value === 'any' && index < window.data.Pin.count) {
         element.style.visibility = 'visible';
       }
     });

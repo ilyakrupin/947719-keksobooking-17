364af939ed7b25bb273dbd7267ce1ed3e5841dd0
Ilya Krupin <dev@krupibox.ru>
2019-07-23 12:12:40 +0300

    Update file module

--- js/file.js
+++ js/file.js
@@ -6,44 +6,53 @@
   var form = document.querySelector('.ad-form');
   var avatarChooser = form.querySelector('#avatar');
   var galleryChooser = form.querySelector('#images');
-  var avatarPreviewContainer = form.querySelector('.ad-form-header__preview');
-  var avatarPreview = form.querySelector('.ad-form-header__preview > img');
+  var avatarContainer = form.querySelector('.ad-form-header__preview');
   var galleryPreview = form.querySelector('.ad-form__photo');
-  var saveAvatarPreview = avatarPreview.cloneNode();
-  var saveGalleryPreview = galleryPreview.cloneNode();
-
+  var avatarPreview = form.querySelector('.ad-form-header__preview > img');
   var galleryContainer = document.querySelector('.ad-form__photo-container');
+  var newAvatar;
+  var oldAvatar;
+  var newGallery;
+  var oldGallery;
 
   window.resetFile = function () {
-    avatarPreviewContainer.removeChild(avatarPreview);
-    var galleryChildren = galleryContainer.querySelectorAll('.ad-form__photo');
-    if (galleryChildren) {
+    if (newAvatar) {
+      newAvatar.remove();
+      avatarContainer.appendChild(oldAvatar);
+    }
+
+    if (newGallery) {
+      var galleryChildren = galleryContainer.querySelectorAll('.ad-form__photo');
       [].forEach.call(galleryChildren, function (element) {
         element.remove();
       });
+      galleryContainer.appendChild(oldGallery);
     }
 
-    avatarPreviewContainer.appendChild(saveAvatarPreview);
-    galleryContainer.appendChild(saveGalleryPreview);
   };
 
   var uploadPicture = function (fileChooser) {
     fileChooser.addEventListener('change', function () {
       var file = fileChooser.files[0];
       var fileName = file.name.toLowerCase();
+
       var matches = FILE_TYPES.some(function (it) {
         return fileName.endsWith(it);
       });
 
       if (matches) {
         var reader = new FileReader();
+
         reader.addEventListener('load', function () {
-          avatarPreview.src = reader.result;
-          avatarPreview.width = '70';
-          avatarPreview.height = '70';
-          avatarPreview.style.borderRadius = '4px';
-          avatarPreview.style.position = 'relative';
-          avatarPreview.style.right = '15px';
+          newAvatar = document.createElement('img');
+          newAvatar.src = reader.result;
+          newAvatar.width = '70';
+          newAvatar.height = '70';
+          newAvatar.style.borderRadius = '4px';
+          newAvatar.style.position = 'relative';
+          newAvatar.style.right = '15px';
+          oldAvatar = avatarContainer.removeChild(avatarPreview);
+          avatarContainer.appendChild(newAvatar);
         });
 
         reader.readAsDataURL(file);
@@ -55,39 +64,42 @@
     fileChooser.addEventListener('change', function () {
       var files = Array.from(fileChooser.files);
       var fragment = document.createDocumentFragment();
-      var renderPreview = function (file) {
-        var clonePreview = galleryPreview.cloneNode();
-        var newGalleryPreview = document.createElement('img');
-        newGalleryPreview.width = '70';
-        newGalleryPreview.height = '70';
-        newGalleryPreview.alt = 'Фотография жилья';
-        newGalleryPreview.style.borderRadius = '4px';
 
-        var fileName = file.name.toLowerCase();
+      var renderPreview = function (thisFile) {
+        newGallery = galleryPreview.cloneNode();
+        var newImage = document.createElement('img');
+        newImage.width = '70';
+        newImage.height = '70';
+        newImage.alt = 'Фотография жилья';
+        newImage.style.borderRadius = '4px';
+
+        var fileName = thisFile.name.toLowerCase();
         var matches = FILE_TYPES.some(function (it) {
           return fileName.endsWith(it);
         });
 
         if (matches) {
           var reader = new FileReader();
+
           reader.addEventListener('load', function () {
-            newGalleryPreview.src = reader.result;
+            newImage.src = reader.result;
           });
-          clonePreview.appendChild(newGalleryPreview);
-          fragment.appendChild(clonePreview);
-          reader.readAsDataURL(file);
+
+          newGallery.appendChild(newImage);
+          fragment.appendChild(newGallery);
+          reader.readAsDataURL(thisFile);
         }
       };
 
-      files.forEach(function (image) {
-        renderPreview(image);
+      files.forEach(function (file) {
+        renderPreview(file);
       });
 
+      oldGallery = galleryContainer.removeChild(galleryPreview);
       galleryContainer.appendChild(fragment);
-      galleryPreview.remove();
     });
   };
 
-  uploadPicture(avatarChooser, avatarPreview);
+  uploadPicture(avatarChooser);
   uploadGallery(galleryChooser);
 })();

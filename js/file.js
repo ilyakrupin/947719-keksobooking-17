0cd079b4caf9a9e499112afaddecbf245a96dadb
Ilya Krupin <dev@krupibox.ru>
2019-07-25 14:44:34 +0300

    Add dnd to gallery

--- js/file.js
+++ js/file.js
@@ -4,69 +4,59 @@
 
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var form = document.querySelector('.ad-form');
-  var avatarChooser = form.querySelector('#avatar');
-  var galleryChooser = form.querySelector('#images');
+  var avatarInput = form.querySelector('#avatar');
+  var photoInput = form.querySelector('#images');
   var avatarContainer = form.querySelector('.ad-form-header__preview');
-  var galleryPreview = form.querySelector('.ad-form__photo');
-  var avatarPreview = form.querySelector('.ad-form-header__preview > img');
+  var imageContainer = form.querySelector('.ad-form__photo');
+  var avatarImage = form.querySelector('.ad-form-header__preview > img');
   var galleryContainer = document.querySelector('.ad-form__photo-container');
-  var newAvatar;
   var oldAvatar;
-  var newGallery;
-  var oldGallery;
+  var newImageContainer;
+  var oldPhoto;
   var dragSourceEl = null;
 
-  window.resetFile = function () {
-    if (newAvatar) {
-      newAvatar.remove();
-      avatarContainer.appendChild(oldAvatar);
-    }
+  var dropArea = form.querySelector('.ad-form__upload');
+  var dropZone = dropArea.querySelector('.ad-form__drop-zone');
+  var dragStyle = dropZone.style.border;
 
-    if (newGallery) {
+  var onDragEnter = function (evt) {
+    evt.preventDefault();
+  };
+
+  var onDragLeave = function (evt) {
+    evt.preventDefault();
+    dropZone.style.border = dragStyle;
+  };
+
+  var onDragOver = function (evt) {
+    evt.preventDefault();
+    dropZone.style.border = 'solid 1px #ff6d51';
+  };
+
+  var onDrop = function (evt) {
+    evt.preventDefault();
+    dropZone.style.border = dragStyle;
+    onPhotoInputChange(evt.dataTransfer.files);
+  };
+
+  dropArea.addEventListener('dragenter', onDragEnter);
+  dropArea.addEventListener('dragleave', onDragLeave);
+  dropArea.addEventListener('dragover', onDragOver);
+  dropArea.addEventListener('drop', onDrop);
+
+  window.resetFile = function () {
+
+    avatarContainer.replaceChild(oldAvatar, avatarImage);
+
+    if (newImageContainer) {
       var galleryChildren = galleryContainer.querySelectorAll('.ad-form__photo');
       [].forEach.call(galleryChildren, function (element) {
         element.remove();
       });
-      galleryContainer.appendChild(oldGallery);
-      oldGallery = null;
+      galleryContainer.appendChild(oldPhoto);
+      oldPhoto = null;
     }
 
-  };
-
-  var uploadPicture = function (fileChooser) {
-    fileChooser.addEventListener('change', function () {
-      var file = fileChooser.files[0];
-      var fileName = file.name.toLowerCase();
-
-      var matches = FILE_TYPES.some(function (it) {
-        return fileName.endsWith(it);
-      });
-
-      if (matches) {
-        var reader = new FileReader();
-
-        reader.addEventListener('load', function () {
-          if (newAvatar) {
-            newAvatar.remove();
-          }
-          newAvatar = document.createElement('img');
-          newAvatar.src = reader.result;
-          newAvatar.width = '70';
-          newAvatar.height = '70';
-          newAvatar.style.borderRadius = '4px';
-          newAvatar.style.position = 'relative';
-          newAvatar.style.right = '15px';
-
-          if (!oldAvatar) {
-            oldAvatar = avatarContainer.removeChild(avatarPreview);
-          }
-
-          avatarContainer.appendChild(newAvatar);
-        });
-
-        reader.readAsDataURL(file);
-      }
-    });
   };
 
   var onImgDragStart = function (evt) {
@@ -74,11 +64,9 @@
     evt.dataTransfer.effectAllowed = 'move';
     evt.dataTransfer.setData('src', dragSourceEl.src);
   };
-
   var onImgDragOver = function (evt) {
     evt.preventDefault();
   };
-
   var onImgDrop = function (evt) {
     var dragTargetEl = evt.target;
     if (dragSourceEl !== dragTargetEl) {
@@ -87,54 +75,85 @@
     }
   };
 
-  var uploadGallery = function (fileChooser) {
-    fileChooser.addEventListener('change', function () {
-      var files = Array.from(fileChooser.files);
-      var fragment = document.createDocumentFragment();
+  var getFiles = function (photoFiles) {
+    var files = Array.from(photoFiles);
 
-      var renderPreview = function (thisFile) {
-        newGallery = galleryPreview.cloneNode();
-        var newImage = document.createElement('img');
-        newImage.width = '70';
-        newImage.height = '70';
-        newImage.alt = 'Фотография жилья';
-        newImage.draggable = true;
-        newImage.style.borderRadius = '4px';
-
-        newImage.addEventListener('dragstart', onImgDragStart);
-        newImage.addEventListener('dragover', onImgDragOver);
-        newImage.addEventListener('drop', onImgDrop);
-
-        var fileName = thisFile.name.toLowerCase();
-        var matches = FILE_TYPES.some(function (it) {
-          return fileName.endsWith(it);
-        });
-
-        if (matches) {
-          var reader = new FileReader();
-
-          reader.addEventListener('load', function () {
-            newImage.src = reader.result;
-          });
-
-          newGallery.appendChild(newImage);
-          fragment.appendChild(newGallery);
-          reader.readAsDataURL(thisFile);
-        }
-      };
-
-      files.forEach(function (file) {
-        renderPreview(file);
+    return files.filter(function (file) {
+      return FILE_TYPES.some(function (it) {
+        return file.name.endsWith(it);
       });
-
-      if (!oldGallery) {
-        oldGallery = galleryContainer.removeChild(galleryPreview);
-      }
-
-      galleryContainer.appendChild(fragment);
     });
   };
 
-  uploadPicture(avatarChooser);
-  uploadGallery(galleryChooser);
+  var getAvatar = function (avatar) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      avatarImage.width = '70';
+      avatarImage.height = '70';
+      avatarImage.style.borderRadius = '4px';
+      avatarImage.style.position = 'relative';
+      avatarImage.style.right = '15px';
+      avatarImage.src = reader.result;
+    });
+    reader.readAsDataURL(avatar);
+
+    return avatarImage;
+  };
+
+  var onAvatarInputChange = function () {
+    var matches = getFiles(arguments[0]);
+
+    if (matches) {
+      oldAvatar = avatarContainer.replaceChild(getAvatar(matches[0]), avatarImage);
+    }
+
+  };
+
+  var getPhoto = function (photo) {
+    var reader = new FileReader();
+    var newImage = document.createElement('img');
+
+    reader.addEventListener('load', function () {
+      newImage.src = reader.result;
+      newImage.width = '70';
+      newImage.height = '70';
+      newImage.alt = 'Фотография жилья';
+      newImage.draggable = true;
+      newImage.style.borderRadius = '4px';
+      newImage.addEventListener('dragstart', onImgDragStart);
+      newImage.addEventListener('dragover', onImgDragOver);
+      newImage.addEventListener('drop', onImgDrop);
+    });
+    reader.readAsDataURL(photo);
+
+    return newImage;
+  };
+
+  var onPhotoInputChange = function () {
+    var matches = getFiles(arguments[0]);
+    var fragment = document.createDocumentFragment();
+
+    if (matches) {
+      matches.forEach(function (file) {
+        newImageContainer = imageContainer.cloneNode();
+        newImageContainer.appendChild(getPhoto(file));
+        fragment.appendChild(newImageContainer);
+      });
+    }
+
+    if (!oldPhoto) {
+      oldPhoto = galleryContainer.removeChild(imageContainer);
+    }
+
+    galleryContainer.appendChild(fragment);
+  };
+
+  avatarInput.addEventListener('change', function () {
+    onAvatarInputChange(avatarInput.files);
+  });
+
+  photoInput.addEventListener('change', function () {
+    onPhotoInputChange(photoInput.files);
+  });
 })();
